b25edc7c2704d4c18a00ecdea3663e7b2fefa299
KhaleghiDev <abolfazlkhaleghi1474@yahoo.com>
2024-06-03 09:41:07 +0330

    add plugin and debug

---
+++ wp-content/plugins/edutsn/edutsn.php
@@ -0,0 +1,16 @@
+<?PHP
+/**
+ * @package edutsn
+ * @version 1.7.2
+ */
+
+
+
+ /*
+ * Plugin Name: edutsn | مدیریت دانشگاه ها 
+ * Description: ثبت دانشگاه ها و مدیریت آنها و مقایسه آنها با هم 
+ * Version: 0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author: itKhaleghi.ir
+ */
